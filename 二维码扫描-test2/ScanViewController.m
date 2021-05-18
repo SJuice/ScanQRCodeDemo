df2d35e42be44c91c2f241bc9a45836a090bf356
SJuice <943383683@qq.com>
2021-05-18 17:45:43 +0800

    11

--- 二维码扫描-test2/ScanViewController.m
+++ 二维码扫描-test2/ScanViewController.m
@@ -196,7 +196,6 @@
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self.navigationController popViewControllerAnimated:YES];
         });
-//        [self.navigationController popViewControllerAnimated:NO];
     }
 }
 
